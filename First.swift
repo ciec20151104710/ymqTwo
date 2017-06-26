b0861dbc150130bfbfce6036fa6c7a6fdeb4ed63
ciec20151104710 <s20151104710@mac26.local>
2017-06-26 16:16:23 +0800

    数据库
    连接数据库

---
+++ First.swift
@@ -0,0 +1,27 @@
+//
+//  First.swift
+//  ymqTwo
+//
+//  Created by s20151104710 on 17/5/22.
+//  Copyright © 2017年 s20151104710. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerfirst: UIViewController {
+    @IBAction func one(_ sender: Any) {
+               
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
